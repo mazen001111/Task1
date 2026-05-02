4a3d69ffbaf59686937c198f8a0b8516572fab19
mazen001111 <2300624@eng.asu.edu.eg>
2026-05-02 16:40:00 +0300

    Add Numeric Format

--- Program.cs
+++ Program.cs
@@ -4,14 +4,14 @@ Console.WriteLine($"Hello Mr {name}\nPlease enter number of small carpets rooms 
 int numberOfSmallCarpets = Convert.ToInt16(Console.ReadLine());
 Console.WriteLine("Please enter number of Large carpets rooms you need to clean :");
 int numberOfLargeCarpets = Convert.ToInt16(Console.ReadLine());
-Console.WriteLine($"the total price of small carpet rooms cleaning is : {numberOfSmallCarpets * 250} $\n");
-Console.WriteLine($"the total price of Large carpet rooms cleaning is : {numberOfLargeCarpets * 350} $\n");
+Console.WriteLine($"the total price of small carpet rooms cleaning is : {numberOfSmallCarpets * 250:C} \n");
+Console.WriteLine($"the total price of Large carpet rooms cleaning is : {numberOfLargeCarpets * 350:C} \n");
 int totalCost = (numberOfSmallCarpets * 250) + (numberOfLargeCarpets * 350);
-Console.WriteLine($"the total cost : {totalCost}");
+Console.WriteLine($"the total cost : {totalCost:C}");
 double totalTax = totalCost * 0.14;
-Console.WriteLine($"Tax: {totalTax} $");
+Console.WriteLine($"Tax: {totalTax:C} ");
 double totalEstimation = (double)totalCost + totalTax;
-Console.WriteLine($"Total estimation : { totalEstimation} $");
+Console.WriteLine($"Total estimation : { totalEstimation:C} ");
 
 
 
